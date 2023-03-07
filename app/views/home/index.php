7c404d4d347f0e6dd1e2a956bc63d9dfa6e16a04
NandoFairuzFakhrizkiAnanda <201220091@unmuhpnk.ac.id>
2023-03-07 12:15:28 +0700

    fix button contact us home

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -2,6 +2,6 @@
     <div class="hero-text-home">
         <p class="mb-0">STEEL FABRICATION</p>
         <p>AND PRECISSION PARTS</p>
-        <a href="" class= "btn btn-primary" >CONTACT US</a>
+        <a href="" class= "btn btn-primary contact-us-home" >CONTACT US</a>
     </div>
 </div>
