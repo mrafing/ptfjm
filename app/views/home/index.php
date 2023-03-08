939a3e402e3d96c5bdd300744b2347e6e4ecfbbb
mrafing <dwpu110300@gmail.com>
2023-03-09 00:14:08 +0700

    final fix home and style

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,7 +1,7 @@
 <div class="hero-home">
     <div class="cnt-hero-home">
         <p class="mb-0 hero-text-home">STEEL FABRICATION</p>
-        <p class="hero-text-home">AND PRECISSION PARTS</p>
+        <p class="mb-5 hero-text-home">AND PRECISSION PARTS</p>
         <a href="" class= "btn btn-primary contact-us-home"><p class="mb-0 contact-us-text-home">CONTACT US</p></a>
     </div>
 </div>
