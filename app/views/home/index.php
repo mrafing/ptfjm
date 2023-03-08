7a009aeda41024652802df82f2988ea706907119
mrafing <dwpu110300@gmail.com>
2023-03-08 17:05:30 +0700

    fix home page and home css

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,7 +1,7 @@
 <div class="hero-home">
-    <div class="hero-text-home">
-        <p class="mb-0">STEEL FABRICATION</p>
-        <p>AND PRECISSION PARTS</p>
-        <a href="" class= "btn btn-primary contact-us-home" >CONTACT US</a>
+    <div class="cnt-hero-home">
+        <p class="mb-0 hero-text-home">STEEL FABRICATION</p>
+        <p class="hero-text-home">AND PRECISSION PARTS</p>
+        <a href="" class= "btn btn-primary contact-us-home"><p class="mb-0 contact-us-text-home">CONTACT US</p></a>
     </div>
 </div>
