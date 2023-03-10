faffdfc20ee0e8ad2089b345bbf51e92395eea4a
mrafing <dwpu110300@gmail.com>
2023-03-10 22:06:33 +0700

    final Home Page

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,7 +1,74 @@
-<div class="hero-home">
+<div class="hero-home mb-5">
     <div class="cnt-hero-home">
         <p class="mb-0 hero-text-home">STEEL FABRICATION</p>
         <p class="mb-5 hero-text-home">AND PRECISSION PARTS</p>
         <a href="" class= "btn btn-primary contact-us-home"><p class="mb-0 contact-us-text-home">CONTACT US</p></a>
+    </div>
+</div>
+<div class="row mb-5">
+    <div class="col">
+        <h2 class="text-center">ABOUT US</h2>
+        <p class="text-center">PT Fytech Jaya Mandiri</p>
+        <div class="row justify-content-center">
+            <p class="col-md-6 text-center">Established since 24 August 2020 . With the deed of establishment no. 12 of 2020, Notary Abdul Aziz S.H., M.KN. PT Fytech Jaya Mandiri is engaged in services, services and manufacturing of precision parts.</p>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-auto">
+                <a href="" class="btn btn-outline-secondary read-more">READ MORE</a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row mb-5">
+        <div class="col mb-3">
+            <h2 class="text-center">OUR TEAM CULTURE</h2>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-sm-6 text-center mb-3">
+                <img src="<?= BASEURL; ?>/img/img1.svg" width="80%">
+            </div>
+            <div class="col-sm-6">
+                <h3 class="mb-0 text-sm-start text-center">We Are A Group Of</h3>
+                <h3 class="mb-2 text-sm-start text-center">Innovative And Creative Minds</h3>
+                <p class="text-sm-start text-center">FJM has an excellent reputation for quality steel fabrication and erection, performance and safety. Our FJM team is highly skilled with all kinds of steel related machining, welding, installation, assembly, repair, maintenance, pumps, piping, motors, demolition and Alignment of Weigh to Scale Conveyors etc. FJM's workshop facilities are spacious enough to be equipped with various machines for steel fabrication, Precision Parts, painting areas and assembly areas. Our workers are highly competent and our Team is highly trained.</p>
+                <div class="row justify-content-sm-start justify-content-center">
+                    <div class="col-auto">
+                        <a href="" class="btn btn-outline-secondary read-more">READ MORE</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row justify-content-center mb-5">
+        <div class="col-sm-7">
+            <h3 class="text-center">WHY FYTECH JAYA MANDIRI?</h3>
+            <p class="text-center">We fundamentally believe in developing long-term strategic partnerships with our clients by exceeding expectations and a great level of transparency. There are countless reasons to choose us and here are some of those that describe us,</p>
+        </div>
+    </div>
+    <div class="row justify-content-between">
+        <div class="col-sm text-center mb-3">
+            <img class="mb-3" src="<?= BASEURL; ?>/img/icon1.svg" alt="" width="50%">
+            <h4>QUALITY</h4>
+            <p>We strive for excellence in all that we do. We set high personal and company standards, and then consistently try to exceed them.</p>
+        </div>
+        <div class="col-sm text-center mb-3">
+            <img class="mb-3" src="<?= BASEURL; ?>/img/icon2.svg" alt="" width="50%">
+            <h4>PASSION</h4>
+            <p>We are passionate and committed in heart and mind to what we do. Our team work around the clock coming up with ideas.</p>
+        </div>
+        <div class="col-sm text-center mb-3">
+            <img class="mb-3" src="<?= BASEURL; ?>/img/icon3.svg" alt="" width="50%">
+            <h4>INTEGRITY</h4>
+            <p>When we face a tough decision we never compromise our values and principles. We do what is right, not what is the easiest.</p>
+        </div>
+        <div class="col-sm text-center mb-3">
+            <img class="mb-3" src="<?= BASEURL; ?>/img/icon4.svg" alt="" width="50%">
+            <h4>EXPERTISE</h4>
+            <p>Our power is consolidated in a group of talented people, led by strong individuals who strive to deliver the finest products to our clients.</p>
+        </div>
     </div>
 </div>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -2,6 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $data['judul']; ?></title>
     <link rel="stylesheet" href="<?= BASEURL; ?>/css/bootstrap.css">
     <link rel="stylesheet" href="<?= BASEURL; ?>/css/style.css">
