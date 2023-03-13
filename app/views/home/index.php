1963dcf02704c69e6eaea5f1cc434698c09c03d3
mrafing <dwpu110300@gmail.com>
2023-03-13 16:13:12 +0700

    home page has done

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,11 +1,12 @@
-<div class="hero-home mb-5">
+<div class="hero-home">
     <div class="cnt-hero-home">
         <p class="mb-0 hero-text-home">STEEL FABRICATION</p>
         <p class="mb-5 hero-text-home">AND PRECISSION PARTS</p>
         <a href="" class= "btn btn-primary contact-us-home"><p class="mb-0 contact-us-text-home">CONTACT US</p></a>
     </div>
 </div>
-<div class="row mb-5">
+
+<div class="about-us-home row mb-5">
     <div class="col">
         <h2 class="text-center">ABOUT US</h2>
         <p class="text-center">PT Fytech Jaya Mandiri</p>
@@ -19,8 +20,9 @@
         </div>
     </div>
 </div>
-<div class="container">
-    <div class="row mb-5">
+
+<div class="our-team-culture-home container">
+    <div class="row">
         <div class="col mb-3">
             <h2 class="text-center">OUR TEAM CULTURE</h2>
         </div>
@@ -42,7 +44,7 @@
     </div>
 </div>
 
-<div class="container">
+<div class="aside-home container">
     <div class="row justify-content-center mb-5">
         <div class="col-sm-7">
             <h3 class="text-center">WHY FYTECH JAYA MANDIRI?</h3>
--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,6 +1,48 @@
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="<?= BASEURL; ?>/js/bootstrap.js"></script>
-<script src="<?= BASEURL; ?>/js/script.js"></script>
-</body>
+
+        <footer>
+            <div class="container pt-5">
+                <div class="row justify-content-between align-items-center mb-5">
+                    <div class="col-sm mb-3">
+                        <div class="row">
+                            <div class="col-auto">
+                                <img src="<?= BASEURL; ?>/img/icon-logo.svg" width="60px">
+                            </div>
+                            <div class="col-9">
+                                <h5 style="color: #fff;">PT. FYTECH JAYA MANDIRI</h5>
+                                <div class="row">
+                                    <div class="col-auto"><img src="<?= BASEURL; ?>/img/map.svg" width="30px"></div>
+                                    <div class="col-8"> <p style="color: #fff;">Jl. Perumahan Bumi Indah ( Ruko Tahap 1 Blok RA No.16 Gelam Jaya Kecamatan Pasar Kemis Kabupaten Tangerang Banten 15560 Indonesia</p></div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-auto"><img src="<?= BASEURL; ?>/img/wa.svg" width="26px"></div>
+                                    <div class="col-8"> <p style="color: #fff;">0858-0628-0914</p></div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-auto"><img src="<?= BASEURL; ?>/img/telp.svg" width="26px"></div>
+                                    <div class="col-8"> <p style="color: #fff;">(021) 59713012</p></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-auto">
+                        <div class="row justify-content-center">
+                            <div class="col-auto"><img src="<?= BASEURL; ?>/img/wa.svg" width="40px"></div>
+                            <div class="col-auto"><img src="<?= BASEURL; ?>/img/in.svg" width="40px"></div>
+                            <div class="col-auto"><img src="<?= BASEURL; ?>/img/ig.svg" width="40px"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row pt-3" style="border-top: 1px solid #fff;">
+                    <div class="col">
+                        <p class="text-center" style="color: #fff;">©2023 Copyright Fytech Jaya Mandiri</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="<?= BASEURL; ?>/js/bootstrap.js"></script>
+        <script src="<?= BASEURL; ?>/js/script.js"></script>
+    </body>
 </html>
